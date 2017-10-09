a2f43997c9253d6fe18d10aebe4d3af9aea1463f
Vaibhav1671998 <vk530873@gmail.com>
2017-10-10 02:31:08 +0530

    Update findCountOfEachChar.c

--- findCountOfEachChar.c
+++ findCountOfEachChar.c
@@ -3,9 +3,9 @@ void find(char *a)
 {
 	int n = strlen(a);
 	int count = 0;
-	int orig[40] = {0}; 
-	int chk[125][10000] = {0};
-	int flag = 0;
+	int orig[40] = {0};  // An arrya to keep count of each character
+	int chk[125][10000] = {0};  // A 2-D array to check whether a character's count has been stored or not
+	int flag = 0; // A variable to check whether that a a character went in a iteration or not
 	for(int i=0;i<n;i++)
 	{
 		count = 0;
@@ -20,7 +20,6 @@ void find(char *a)
 					orig[((int)a[i])-((int)'a')] = orig[((int)a[i])-((int)'a')]+1;
 					flag = 1;	
 				}
-				// orig[((int)a[i])-((int)'a')] = orig[((int)a[i])-((int)'a')]+1;
 			}
 		}
 		chk[(int)a[i]][((int)a[i])-((int)'a')] = 1;
@@ -28,7 +27,6 @@ void find(char *a)
 		{
 			printf("%c \t %d\n",a[i],orig[((int)a[i])-((int)'a')]);
 		}
-		// printf("%d\n",orig[((int)a[i])-((int)'a')]);
 	}
 }
 int main(int argc, char const *argv[])
